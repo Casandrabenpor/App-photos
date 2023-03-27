0894bb4ca84d4b0bd604e613a091af255f76a8da
casandrabenpor <casandra211@gmail.com>
2023-03-27 18:55:31 +0200

    second commit

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -3,8 +3,8 @@ import "./search.css";
 export const Search = () => {
   return (
     <form  className="search-form">
-      <input type="text" className="search" />
-      <button type="submit" placeholder="Search" className="search-button">
+      <input type="text" placeholder="Search"className="search" />
+      <button type="submit"  className="search-button">
         🔎
       </button>
     </form>
