c2d977bdec768130e2abf1d81fbea7c70e5ce3fb
casandrabenpor <casandra211@gmail.com>
2023-03-30 12:39:59 +0200

    border

--- src/features/photoSlice/apiThunk.js
+++ src/features/photoSlice/apiThunk.js
@@ -4,7 +4,7 @@ export const loadPhotos = createAsyncThunk("photos/getPhotos", async (queryValue
     console.log(queryValue);
     
     const data = await fetch(
-        `https://api.unsplash.com/photos/random?client_id=c6-Zu3LQmTu2l5yW5lDfXq2L2Wpg1Q7f0kF286entVg&count=30&query=${queryValue}`
+        `https://api.unsplash.com/photos/random?client_id=c6-Zu3LQmTu2l5yW5lDfXq2L2Wpg1Q7f0kF286entVg&count=10&query=${queryValue}`
     ).catch((error) => {
         throw error;
     });
